a9847b97ec3a83563b6b2bbbbff76aa41b1a654a
kevinzhou <2269809137@qq.com>
2024-06-12 18:30:39 +0800

    111

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -65,7 +65,7 @@ async function createWindow() {
   if (VITE_DEV_SERVER_URL) { // #298
     win.loadURL(VITE_DEV_SERVER_URL)
     // Open devTool if the app is not packaged
-    win.webContents.openDevTools()
+    // win.webContents.openDevTools()
   } else {
     win.loadFile(indexHtml)
   }
--- src/App.tsx
+++ src/App.tsx
@@ -1,89 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import {
-  MenuFoldOutlined,
-  MenuUnfoldOutlined,
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-} from '@ant-design/icons';
-import { Button, Layout, Menu, theme } from 'antd';
-import { invokeIpcMethod } from './utils/ipcUtils';
+import React from 'react';
+import LoginPage, { Username, Password, Submit, Title, Logo, Reset } from '@react-login-page/page1';
+import LoginLogo from 'react-login-page/logo';
 
-const { Header, Sider, Content } = Layout;
+const styles = { height: "100vh",width:"100vw" };
 
-const App: React.FC = () => {
-  const [collapsed, setCollapsed] = useState(false);
-  const {
-    token: { colorBgContainer, borderRadiusLG },
-  } = theme.useToken();
+const Demo = () => (
+  <div style={styles}>
+    <LoginPage>
+      <Username name="userUserName" />
+      <Password placeholder="请输入密码" name="userPassword" />
+      <Submit>提交</Submit>
+      <Reset disabled>重置</Reset>
+      <Title />
+      <Logo>
+        <LoginLogo />
+      </Logo>
+    </LoginPage>
+  </div>
+);
 
-  useEffect(() => {
-    const getAreaList=async()=>{
-      const mysql =await invokeIpcMethod("mysqlQuery","SELECT * from tz_basket tb ")
-      console.log(mysql);
-    }
-    getAreaList();
-  }, [])
-
-  return (
-    <Layout>
-      <Button onClick={async () => {
-        const mysql = await invokeIpcMethod('SELECT * from tz_area ta ')
-        console.log(mysql);
-
-      }}>mysql</Button>
-      <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="demo-logo-vertical" />
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={['1']}
-          items={[
-            {
-              key: '1',
-              icon: <UserOutlined />,
-              label: 'nav 1',
-            },
-            {
-              key: '2',
-              icon: <VideoCameraOutlined />,
-              label: 'nav 2',
-            },
-            {
-              key: '3',
-              icon: <UploadOutlined />,
-              label: 'nav 3',
-            },
-          ]}
-        />
-      </Sider>
-      <Layout>
-        <Header style={{ padding: 0, background: colorBgContainer }}>
-          <Button
-            type="text"
-            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-            onClick={() => setCollapsed(!collapsed)}
-            style={{
-              fontSize: '16px',
-              width: 64,
-              height: 64,
-            }}
-          />
-        </Header>
-        <Content
-          style={{
-            margin: '24px 16px',
-            padding: 24,
-            minHeight: 280,
-            background: colorBgContainer,
-            borderRadius: borderRadiusLG,
-          }}
-        >
-          Content
-        </Content>
-      </Layout>
-    </Layout>
-  );
-};
-
-export default App;
+export default Demo;
